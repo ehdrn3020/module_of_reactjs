e6f9e305a9682bfe1ec1c2446e1be294f79ef49b
DongGu Kang <ehdrnsla3020@naver.com>
2023-08-18 09:24:03 +0900

    Delete typescript_reactjs directory

--- typescript_reactjs/src/components/Object.tsx
+++
@@ -1,5 +0,0 @@
-import { ObjectProps } from './Object.types'
-
-export const Object = (props: ObjectProps) => {
-    return <div>{props.name.first} {props.name.last}</div>
-}
--- typescript_reactjs/src/components/Object.types.ts
+++
@@ -1,6 +0,0 @@
-export type ObjectProps = {
-    name: {
-        first: string
-        last: string
-    }
-}
