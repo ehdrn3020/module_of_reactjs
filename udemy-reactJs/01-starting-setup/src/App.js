1136dbefe19f8bac58c63866ed6e80d82a823e99
ehdrn3020 <ehdrnsla3020@naver.com>
2023-08-24 06:57:36 +0900

    20230824 add input Expense form

--- udemy-reactJs/01-starting-setup/src/App.js
+++ udemy-reactJs/01-starting-setup/src/App.js
@@ -1,18 +1,22 @@
-import './components/Expenses/Expenses.css'
+import "./components/Expenses/Expenses.css";
 import ExpenseItem from "./components/Expenses/ExpenseItem";
-import Expenses from './components/Expenses/Expenses';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
-  const expense = [
-    { date: new Date(2023, 7, 15), title: "차량 수리비", amount: 230000 },
-    { date: new Date(2023, 7, 11), title: "영화관", amount: 20000 },
-    { date: new Date(2023, 7, 5), title: "점심 값", amount: 15000 },
-    { date: new Date(2023, 6, 30), title: "차량 유류비", amount: 95000 },
-  ];
+    const expense = [
+        { date: new Date(2023, 7, 15), title: "차량 수리비", amount: 230000 },
+        { date: new Date(2023, 7, 11), title: "영화관", amount: 20000 },
+        { date: new Date(2023, 7, 5), title: "점심 값", amount: 15000 },
+        { date: new Date(2023, 6, 30), title: "차량 유류비", amount: 95000 },
+    ];
 
-  return (
-    <Expenses expense={expense} />
-  );
+    return (
+        <div>
+            <NewExpense />
+            <Expenses expense={expense} />
+        </div>
+    );
 }
 
 export default App;
