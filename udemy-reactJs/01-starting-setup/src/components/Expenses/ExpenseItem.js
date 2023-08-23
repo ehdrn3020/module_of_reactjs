c7d2b241af668fe9be9d288452ab3d74e6f2b911
ehdrn3020 <ehdrnsla3020@naver.com>
2023-08-24 06:17:44 +0900

    20230824 add useState

--- udemy-reactJs/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ udemy-reactJs/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  }
   
   return (
     <Card className="expense-item">
@@ -10,9 +17,10 @@ function ExpenseItem(props) {
         date={props.date}
       />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}원</div>
       </div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
