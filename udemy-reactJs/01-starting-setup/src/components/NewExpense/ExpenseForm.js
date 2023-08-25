8428a3a95d4090127d47e9150e3b83930d1cc7ca
ehdrn3020 <ehdrnsla3020@naver.com>
2023-08-25 09:21:46 +0900

    20230825 eventLister를 이용한 useState 활용

--- udemy-reactJs/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ udemy-reactJs/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
+    const titleCangeHandler = (event) => {
+        setUserInput({
+            ...userInput, // spread 연산자 사용
+            enteredTitle: event.target.value
+        })
+    };
+    const amountCangeHandler = (event) => {
+        setUserInput({
+            ...userInput, // spread 연산자 사용
+            enteredAmount: event.target.value
+        })
+    };
+    const dateCangeHandler = (event) => {
+        setUserInput({
+            ...userInput, // spread 연산자 사용
+            enteredDate: event.target.value
+        })
+    };
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>타이틀</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleCangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>비용</label>
-                    <input type="number" min='0.01' step='0.01' />
+                    <input
+                        type="number"
+                        min="0.01"
+                        step="0.01"
+                        onChange={amountCangeHandler}
+                    />
                 </div>
                 <div className="new-expense__control">
                     <label>날짜</label>
-                    <input type="date" min='2023-01-01' max='2023-12-31' />
+                    <input
+                        type="date"
+                        min="2023-01-01"
+                        max="2023-12-31"
+                        onChange={dateCangeHandler}
+                    />
                 </div>
             </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>등록하기</button>
+            <div className="new-expense__actions">
+                <button type="submit">등록하기</button>
             </div>
         </form>
     );
