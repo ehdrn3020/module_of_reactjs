e83355d201f919dc702ff0593244d9184ace97e2
ehdrn3020 <ehdrnsla3020@naver.com>
2023-08-30 09:32:29 +0900

    20230830 날자 select 추가

--- udemy-reactJs/01-starting-setup/src/components/Expenses/Expenses.js
+++ udemy-reactJs/01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,15 +1,28 @@
-import './Expenses.css';
-import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+import React, { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 
 function Expenses(props) {
-    return (
-        <Card className="expenses">
-          {props.expense.map((item) => (
-            <ExpenseItem date={item.date} title={item.title} amount={item.amount} />
-          ))}
-        </Card>
-    );
+  const [filteredYear, setFilteredYear] = useState("2023");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        {props.expense.map((item) => (
+          <ExpenseItem
+            date={item.date}
+            title={item.title}
+            amount={item.amount}
+          />
+        ))}
+      </Card>
+    </div>
+  );
 }
 
 export default Expenses;
