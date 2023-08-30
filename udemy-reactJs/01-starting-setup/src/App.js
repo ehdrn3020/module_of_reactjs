d2cf83ffa041e4bb618c264b7126920de83e9a15
ehdrn3020 <ehdrnsla3020@naver.com>
2023-08-31 07:26:15 +0900

    20230831 배열동적으로 추가

--- udemy-reactJs/01-starting-setup/src/App.js
+++ udemy-reactJs/01-starting-setup/src/App.js
@@ -1,23 +1,28 @@
+import React, {useState} from 'react';
 import "./components/Expenses/Expenses.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummy_items = [
+    { date: new Date(2023, 7, 15), title: "차량 수리비", amount: 230000 },
+    { date: new Date(2023, 7, 11), title: "영화관", amount: 20000 },
+    { date: new Date(2023, 7, 5), title: "점심 값", amount: 15000 },
+    { date: new Date(2023, 6, 30), title: "차량 유류비", amount: 95000 },
+];
+
 function App() {
-    const expense = [
-        { date: new Date(2023, 7, 15), title: "차량 수리비", amount: 230000 },
-        { date: new Date(2023, 7, 11), title: "영화관", amount: 20000 },
-        { date: new Date(2023, 7, 5), title: "점심 값", amount: 15000 },
-        { date: new Date(2023, 6, 30), title: "차량 유류비", amount: 95000 },
-    ];
+    const [expenses, setExpenses] = useState(dummy_items);
 
     const addExpenseHandler = expense => {
         console.log('in App.js');
-        console.log(expense);
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses]
+        });
     }
     return (
         <div>
             <NewExpense onAddExpense={addExpenseHandler} />
-            <Expenses expense={expense} />
+            <Expenses expense={expenses} />
         </div>
     );
 }
--- udemy-reactJs/01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ udemy-reactJs/01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
 
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  }
   
   return (
     <Card className="expense-item">
@@ -17,10 +12,9 @@ function ExpenseItem(props) {
         date={props.date}
       />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}원</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
